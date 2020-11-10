e2ce62e42186bf2d59395ca33d44df6ef4ee8993
Nhat Tan <tanvu@mecar.vn>
2020-11-10 17:08:50 +0700

    add font google

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,26 +9,47 @@ module.exports = {
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
-      option: {
+      options: {
         name: `pages`,
         path: `${__dirname / src / pages}`,
       },
     },
     {
       resolve: `gatsby-source-filesystem`,
-      option: {
+      options: {
         name: `posts`,
         path: `${__dirname / src / posts}`,
       },
     },
     {
       resolve: `gatsby-source-filesystem`,
-      option: {
+      options: {
         name: `images`,
         path: `${__dirname / src / images}`,
       },
     },
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.md`, `.mdx`],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1200,
+            },
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [`roboto mono`, `multi\:400,400i,700,700i`],
+        display: "swap",
+      },
+    },
   ],
 }
